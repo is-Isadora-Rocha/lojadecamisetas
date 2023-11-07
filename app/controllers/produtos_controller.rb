8e67ebdc423dc4b56e27a967728e600902a796d3
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-11-07 17:45:04 -0300

    limitando as ordens dos proutos por nome e preco

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,5 +1,8 @@
 class ProdutosController < ApplicationController
     def index
-        @todos = Produto.order :nome
+        # limit 5 = são os 5 nomes de produtos mais baratos
+        @produtos_por_nome = Produto.order(:nome).limit 5
+        @produtos_por_preco = Produto.order(:preco).limit 2
+        # limit 2 = são os 2 preços de produtos mais baratos
     end
 end
