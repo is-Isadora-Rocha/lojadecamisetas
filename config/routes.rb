b08e59c68cc9e935e0b877dc5da0a8249338ab3f
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-11-08 10:18:45 -0300

    reajustes no html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  get "produtos" => "produtos#index"
+  # get "produtos" => "produtos#index"
+  root "produtos#index"
+  # "produtos" é o endereço principal no root/raiz
 
 end
