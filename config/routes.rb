da580285c383fd5dc17e0efcb926a3a1e8cdda61
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-11-09 08:19:05 -0300

    html para ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  post "/produtos" => "produtos#create"
+  # post "/produtos" => "produtos#create"
+  post "/produtos", to: "produtos#create"
   get "/produtos/new" => "produtos#new"
   root "produtos#index"
 
