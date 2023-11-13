2430910cb8ba86b57df1d8e7e31e4cefe45b9a43
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-11-13 11:16:47 -0300

    implementação de buscas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+=begin
 
-  # post "/produtos" => "produtos#create"
   post "/produtos", to: "produtos#create"
   get "/produtos/new" => "produtos#new"
   delete "/produtos/:id" => "produtos#destroy", as: :produto
+
+=end
+
+# resources: gera todos os http
+  resources :produtos, only: [:new, :create, :destroy]
   root "produtos#index"
 
 end
