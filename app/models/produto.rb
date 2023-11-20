5bdf8c4faf0c656d23abf77ad2bc12d17a7cc81f
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-11-20 17:46:09 -0300

    are u sure?

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,6 +1,6 @@
 class Produto < ApplicationRecord
 
-    belongs_to :departamento
+    belongs_to :departamentos
 
     validates :quantidade, presence: true
     validates :nome, length: { minimum: 5}
